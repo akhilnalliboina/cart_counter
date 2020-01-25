f67610fbb0a9fe3c8a6f1d80f6a1aa276d0de8f0
akhiln95 <akhilnalliboina@gmail.com>
2020-01-25 20:44:16 +0100

    Updates

---
+++ precache-manifest.3d221e78fc3e5f15e229c79f8835928b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9c90eed9307e7d32f7809f41eeefd3",
+    "url": "/cart_counter/index.html"
+  },
+  {
+    "revision": "11efb726f91d27a0e45e",
+    "url": "/cart_counter/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "11efb726f91d27a0e45e",
+    "url": "/cart_counter/static/js/2.bbfe1bdb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cart_counter/static/js/2.bbfe1bdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02d57183be3c33f40427",
+    "url": "/cart_counter/static/js/main.b95f0165.chunk.js"
+  },
+  {
+    "revision": "6da34dd7c3cd03f11ff3",
+    "url": "/cart_counter/static/js/runtime-main.72cfe43a.js"
+  }
+]);
